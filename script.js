577d424233d3f031b8ff75b01e26a01892250292
t3nitramz <45600901+t3nitramz@users.noreply.github.com>
2018-12-04 17:04:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("#lightSlider").lightSlider({
+      item: 1,
+      slideMove: 1, // slidemove will be 1 if loop is true
+      slideMargin: 10,
+      loop: true,
+      adaptiveWidth: true
+    });
+
+})
